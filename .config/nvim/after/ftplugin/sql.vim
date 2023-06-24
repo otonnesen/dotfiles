db0dae2b9f4d96baa303e37c73f3af4faf0d4f2c
Oliver Tonnesen <oliver.tonnesen@panienergy.com>
2023-06-24 03:22:54 -0700

    Comment out vsnip; replace sqlformat with pg_format

--- .config/nvim/after/ftplugin/sql.vim
+++ .config/nvim/after/ftplugin/sql.vim
@@ -1,1 +1,2 @@
-nnoremap <F3> :%!sqlformat -k "lower" --reindent --comma_first True -<CR>
+" nnoremap <F3> :%!sqlformat -k "lower" --reindent --comma_first True -<CR>
+nnoremap <F3> :%!pg_format 2>/dev/null --type-case 1 --keyword-case 1 --comma-start --wrap-comment<CR>
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,7 +9,7 @@ require("config.telescope")
 require("config.lualine")
 require("config.neorg")
 -- require("config.vimwiki")
-require("config.vsnip")
+-- require("config.vsnip")
 require("config.null-ls")
 require("config.nvim-tree")
 require("config.orgmode")
@@ -20,6 +20,7 @@ require("config.mason")
 require("config.lsp")
 require("config.gruvbox")
 require("config.gitsigns")
+require("config.slime")
 
 -- This is last so it takes precedence over any config done by plugins. I'm
 -- pretty sure the italic operator highlight is probably done by treesitter, so
